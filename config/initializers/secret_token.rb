e47e96392ed8a237f18014bb4aac5e1542ee4c19
Carlos Fernandez <cfern1990@gmail.com>
2012-09-29 00:31:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GraphicsCompare::Application.config.secret_token = 'dbf1811d31996c647850c8815d04005641849fa3fcc3cac25b78535025e68b758d8dde894c6e0dc4d19c5fd0fa3008c82bd35229f14ab68d21e22b606013419c'
